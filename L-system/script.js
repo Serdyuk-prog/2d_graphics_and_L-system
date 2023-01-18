d47d359458f756f802fd63b16a2487d46f74c068
Serdyuk-prog <tony.not.gloomy@gmail.com>
2023-01-19 00:21:38 +0700

    done

--- L-system/script.js
+++ L-system/script.js
@@ -1,10 +1,9 @@
 class Rules {
-    constructor(S, F, X, Y, a) {
+    constructor(S, F, X, Y) {
         this.S = S;
         this.F = F;
         this.X = X;
         this.Y = Y;
-        this.a = a;
     }
 }
 
@@ -15,20 +14,28 @@ class Point {
     }
 }
 
-const commands = "F"
-
-const startPoint = new Point(250, 250);
-let curr_angle = 270;
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-const line_length = 50;
-let curr_buffer = null;
-let curr_point = startPoint;
+const commands = "Fb+-[]";
 
-let S = "F";
-let F = "F+F−F−F+F";
+// params
+let startPoint = new Point(0, 0);
+let line_length = 5;
+let curr_point = null;
+let angle_change = 0;
+let cur_gen = null;
+let S = "";
+let F = "";
+let X = "";
+let Y = "";
+let new_r = null;
 
-let new_r = new Rules(S, F, null, null, 20);
+// variables
+let curr_angle = 0;
+let curr_buffer = [];
+let gen_num = 0;
+
+// let new_r = new Rules(S, F, null, null, 20);
 
 function new_gen(data, rules) {
     let result = "";
@@ -46,15 +53,23 @@ function toRadian(degree) {
     return (degree * Math.PI) / 180;
 }
 
-function drawLine(point) {
-    const x1 = point.x;
-    const y1 = point.y;
+function drawLine() {
+    const x1 = curr_point.x;
+    const y1 = curr_point.y;
     const r = line_length;
     const theta = toRadian(curr_angle); // strait up
     ctx.moveTo(x1, y1);
     ctx.lineTo(x1 + r * Math.cos(theta), y1 + r * Math.sin(theta));
     ctx.stroke();
-    return new Point(x1 + r * Math.cos(theta), y1 + r * Math.sin(theta));
+    curr_point = new Point(x1 + r * Math.cos(theta), y1 + r * Math.sin(theta));
+}
+
+function movePoint() {
+    const x1 = curr_point.x;
+    const y1 = curr_point.y;
+    const r = line_length;
+    const theta = toRadian(curr_angle);
+    curr_point = new Point(x1 + r * Math.cos(theta), y1 + r * Math.sin(theta));
 }
 
 function rotate(angle) {
@@ -62,34 +77,168 @@ function rotate(angle) {
 }
 
 function buffer() {
-    curr_buffer = curr_point;
+    curr_buffer.push([curr_point, curr_angle]);
 }
 
 function clearBuffer() {
-    curr_buffer = null;
+    curr_buffer = [];
 }
 
-function draw_main(commands){
-    for(let command of commands){
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    curr_point = startPoint;
+    clearBuffer();
+}
 
+function draw_main(commands) {
+    clearCanvas();
+    console.log(commands);
+    for (let command of commands) {
+        if (command == "F") {
+            drawLine();
+        }
+        if (command == "b") {
+            movePoint();
+        }
+        if (command == "+") {
+            rotate(angle_change);
+        }
+        if (command == "-") {
+            rotate(angle_change * -1);
+        }
+        if (command == "[") {
+            buffer();
+        }
+        if (command == "]") {
+            let buffer = curr_buffer.pop();
+            curr_point = buffer[0];
+            curr_angle = buffer[1];
+        }
     }
 }
 
+function draw_gen() {
+    draw_main(cur_gen);
+    cur_gen = new_gen(cur_gen, new_r);
+    gen_num++;
+}
 
-console.log(new_gen("F+F−F−F+F", new_r));
+const draw_button = document.querySelector("#draw-button");
+draw_button.addEventListener("click", () => {
+    // printState()
+    if (gen_num > 5) {
+        alert("Too complex!");
+    } else {
+        draw_gen();
+    }
+});
 
-// x1 = 250;
-// y1 = 250;
-// r = 50;
-// theta = toRadian(270); // strait up
-// ctx.moveTo(x1, y1);
-// ctx.lineTo(x1 + r * Math.cos(theta), y1 + r * Math.sin(theta));
-// ctx.stroke();
-let point = startPoint;
-point = drawLine(point);
-rotate(90);
-point = drawLine(point);
-rotate(90);
-point = drawLine(point);
-rotate(90);
-point = drawLine(point);
+const s_input = document.querySelector("#data-s");
+const f_input = document.querySelector("#data-f");
+const x_input = document.querySelector("#data-x");
+const y_input = document.querySelector("#data-y");
+const a_input = document.querySelector("#data-a");
+const stroke_input = document.querySelector("#data-stroke");
+const x0_input = document.querySelector("#data-x0");
+const y0_input = document.querySelector("#data-y0");
+
+function setDefaults() {
+    s_input.value = "F";
+    f_input.value = "F+F-F-F+F";
+    x_input.value = null;
+    y_input.value = null;
+    a_input.value = 90;
+    stroke_input.value = 10;
+    x0_input.value = 0;
+    y0_input.value = 0;
+}
+
+function getParams() {
+    clearCanvas();
+    gen_num = 0;
+    let x = parseFloat(x0_input.value);
+    let y = parseFloat(y0_input.value);
+    startPoint = new Point(x, y);
+    line_length = parseFloat(stroke_input.value);
+    curr_point = startPoint;
+    angle_change = parseFloat(a_input.value);
+    S = s_input.value;
+    F = f_input.value;
+    X = x_input.value;
+    Y = y_input.value;
+    cur_gen = S;
+    new_r = new Rules(S, F, X, Y);
+}
+
+function printState() {
+    console.log(S, F, X, Y);
+    console.log(line_length, curr_point, angle_change);
+    console.log(cur_gen);
+    console.log(gen_num);
+    console.log(new_r.S, new_r.F, new_r.X, new_r.Y);
+}
+
+setDefaults();
+getParams();
+
+const inputs = document.querySelectorAll("input");
+inputs.forEach((el) => {
+    el.addEventListener("input", () => {
+        getParams();
+    });
+});
+
+function preset2() {
+    s_input.value = "F++F++F";
+    f_input.value = "F-F++F-F";
+    x_input.value = null;
+    y_input.value = null;
+    a_input.value = 60;
+    stroke_input.value = 5;
+    x0_input.value = 250;
+    y0_input.value = 250;
+}
+
+function preset3() {
+    s_input.value = "X";
+    f_input.value = "F";
+    x_input.value = "-YF+XFX+FY-";
+    y_input.value = "+XF-YFY-FX+";
+    a_input.value = 90;
+    stroke_input.value = 10;
+    x0_input.value = 0;
+    y0_input.value = 500;
+}
+
+function preset4() {
+    s_input.value = "F";
+    f_input.value = "F[+F]F[-F][F]";
+    x_input.value = null;
+    y_input.value = null;
+    a_input.value = 20;
+    stroke_input.value = 10;
+    x0_input.value = 0;
+    y0_input.value = 250;
+}
+
+document.querySelector("#preset-1").addEventListener("click", () => {
+    setDefaults();
+    getParams();
+});
+
+document.querySelector("#preset-2").addEventListener("click", () => {
+    preset2();
+    getParams();
+});
+
+document.querySelector("#preset-3").addEventListener("click", () => {
+    preset3();
+    getParams();
+});
+
+document.querySelector("#preset-4").addEventListener("click", () => {
+    preset4();
+    getParams();
+});
+
